80af3bdafd6aa30fe59128cebf177d1e994dfaa9
Marc Pastor <53482789+marctotti5@users.noreply.github.com>
2019-09-07 14:15:55 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-# run_analysis
-Final project of Cleaning and Tyding Data in R from Coursera
 getwd()
 setwd("C:/Users/marct/OneDrive - Tecnocampus Mataro-Maresme/Documentos/CURSOS/R PATH COURSERA/Cleaning & Tidying Data in R/Week 4/UCI HAR Dataset")
 
@@ -14,46 +12,42 @@ subject_train <- read.table("C:/Users/marct/OneDrive - Tecnocampus Mataro-Maresm
 xtest <- read.table("././test/X_test.txt", sep="", col.names=features$functions)
 xtrain <- read.table("././train/X_train.txt", sep="", col.names=features$functions)
 xmerged <- rbind(xtest, xtrain)
-View(xmerged)
-
-library(tidyverse)
 
 ytest <- read.table("././test/y_test.txt", sep="")
 ytrain <- read.table("././train/y_train.txt", sep="")
 ymerged <- rbind(ytest, ytrain)
+
 colnames(ymerged) <- "code"
 ymerged <- merge(ymerged, activities, by="code")
-ymerged %>% group_by(activity) %>% summarise(count=n())
+
 
 subjectmerged <- rbind(subject_test, subject_train)
-
-nrow(xymerged)
 xymerged <- cbind(xmerged, ymerged, subjectmerged)
-nrow(xymerged)
-colnames(xymerged)
 
 xymerged2 <- xymerged[, c(562, 563, 564, 1:561)]
 xymerged2$subject
 
 View(xymerged2)
 
-# 2. Find out the mean and sd for each variable
+#2. Find out the mean and sd for each variable
 a <- function(x){
         output <- data.frame()
         means <- numeric()
-        deviations <- numeric()
+        sd <- numeric()
         for(i in seq_along(x)){
                 names <- colnames(x)
                 means <- c(means, mean(x[,i], na.rm=TRUE))
-                deviations <- c(deviations, sd(x[,i], na.rm=TRUE))
+                sd <- c(sd, sd(x[,i], na.rm=TRUE))
         }
-        output <- cbind(names, means, deviations)
-        output
+        output <- cbind(names, means, sd)
+        as.data.frame(output[-1:-3,])
 }
 
 t <- a(xymerged2)
+t
+#3. Find out average for each variable for each activity and each subject
+xymerged_grouped3 <- xymerged2 %>% group_by(activity, subject) %>% summarise_all("mean") %>% arrange(subject)
+xymerged_grouped3
 
-#4. Find out average for each variable for each activity and each subject
-xymerged_grouped3 <- xymerged2 %>% group_by(activity, subject) %>% summarise_all("mean")
-
+write.table(xymerged_grouped3, row.name=F, file="runanalysis.txt")
 
